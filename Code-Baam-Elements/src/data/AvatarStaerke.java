4d116a92e2e5a78ee7bc93b749ee9fba08b70642
simonjaeggi <dev.simon.jaeggi@gmail.com>
2018-03-26 15:09:37 +0200

    Staerkenvergleich der Avatare implementiert

--- Code-Baam-Elements/src/data/AvatarStaerke.java
+++ Code-Baam-Elements/src/data/AvatarStaerke.java
@@ -1,6 +1,7 @@
 package data;
 
-import logic.Avatar;
+import logic.*;
+
 
 public class AvatarStaerke {
 	private Avatar Avatar_1;
@@ -11,7 +12,34 @@ public class AvatarStaerke {
 	}
 	public Avatar Staerkenvergleich(){
 		Avatar zw = null;
-			if(Avatar_1.get)
+			if(Avatar_1 instanceof Feuerelementar || Avatar_1 instanceof Erdelementar && Avatar_2 instanceof Feuerelementar || Avatar_2 instanceof Erdelementar){
+				if(Avatar_1 instanceof Feuerelementar){
+					return Avatar_1;
+				}else if(Avatar_2 instanceof Feuerelementar){
+					return Avatar_2;
+				}
+			}
+			else if(Avatar_1 instanceof Erdelementar || Avatar_1 instanceof Luftelementar && Avatar_2 instanceof Erdelementar || Avatar_2 instanceof Luftelementar){
+				if(Avatar_1 instanceof Erdelementar){
+					return Avatar_1;
+				}else if(Avatar_2 instanceof Erdelementar){
+					return Avatar_2;
+				}
+			}
+			else if(Avatar_1 instanceof Luftelementar || Avatar_1 instanceof Wasserelementar && Avatar_2 instanceof Luftelementar || Avatar_2 instanceof Wasserelementar){
+				if(Avatar_1 instanceof Luftelementar){
+					return Avatar_1;
+				}else if(Avatar_2 instanceof Luftelementar){
+					return Avatar_2;
+				}
+			}
+			else if(Avatar_1 instanceof Wasserelementar || Avatar_1 instanceof Feuerelementar && Avatar_2 instanceof Wasserelementar || Avatar_2 instanceof Feuerelementar){
+				if(Avatar_1 instanceof Wasserelementar){
+					return Avatar_1;
+				}else if(Avatar_2 instanceof Wasserelementar){
+					return Avatar_2;
+				}
+			}
 		
 		return zw;
 	}
