91729e6e036e4c7cd977168ad8ed2ccb10eaec71
simonjaeggi <dev.simon.jaeggi@gmail.com>
2018-03-26 15:25:30 +0200

    test changend.

--- Code-Baam-Elements/src/logic/test.java
+++ Code-Baam-Elements/src/logic/test.java
@@ -4,7 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Kampf s = new Kampf(new Feuerelementar(), new Luftelementar());
+	//	Kampf s = new Kampf(new Feuerelementar(), new Luftelementar());
 		System.out.println("test");
 	}
 
