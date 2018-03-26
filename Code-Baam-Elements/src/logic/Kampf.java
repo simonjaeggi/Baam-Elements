72c9828692bb498ce6fb1defd092328ecdad3d92
simonjaeggi <dev.simon.jaeggi@gmail.com>
2018-03-26 15:24:58 +0200

    Kampf bis zu einer minimalen Funktionsweise ausprogrammiert.

--- Code-Baam-Elements/src/logic/Kampf.java
+++ Code-Baam-Elements/src/logic/Kampf.java
@@ -18,15 +18,34 @@ public class Kampf {
 	private void kaempfen(){
 		
 		double zw = Avatar_1.AngreifenAufrufen();
+		if(Avatar_1 == StarkerAvatar){
+			zw = zw * 1.2;
+		}
+		if (gestorben(Avatar_2) == true){
+			gewonnen();
+			
+		}
+		zw = Avatar_2.AngreifenAufrufen();
+		if(Avatar_2 == StarkerAvatar){
+			zw = zw * 1.2;
+		}
+		if (gestorben(Avatar_1) == true){
+			verloren();
+			
+		}
 		
 	}
 	private boolean gestorben(Avatar avatar){
-		return true;
+
+		if (avatar.getLebenspunkte() <= 1){
+			return true;
+		}
+		return false;
 	}
 	private void gewonnen(){
-
+		System.out.println("Yaay, you won!");
 	}
 	private void verloren(){
-
+		System.out.println("Awww, you lost!");
 	}
 }
