177acba62be161a876ac4e3cdc1beeab80a9ce43
simonjaeggi <dev.simon.jaeggi@gmail.com>
2018-03-27 16:53:43 +0200

    AuswahlAvatar zur funktionalität ausprogrammiert + Javadoc.

--- Code-Baam-Elements/src/logic/AuswahlAvatar.java
+++ Code-Baam-Elements/src/logic/AuswahlAvatar.java
@@ -1,12 +1,34 @@
 package logic;
+import java.util.Random;
 
+/**
+ * 
+ * @author Simon Jäggi, Lukas Reinhardt
+ * @version 0.1 
+ * Wählt zufällig einen Avatar aus.
+  */
 public class AuswahlAvatar {
 	private Avatar Feuerelementar = new Feuerelementar();
 	private Avatar Wasserelementar = new Wasserelementar();
 	private Avatar Luftelementar = new Luftelementar();
 	private Avatar Erdelementar = new Erdelementar();
 
-	public short RandomGegner(){
-		return 0;
+	/**
+	 * 
+	 * @return Avatar
+	 * Gibt einen zufällig ausgewählten Avatar zurück.
+	 */
+	public Avatar RandomGegner(){
+		Random rand = new Random();
+		int rz = rand.nextInt((4 - 1) + 1) + 1;
+
+		switch (rz){
+		case 1:	return Feuerelementar;
+		case 2: return Wasserelementar;
+		case 3: return Luftelementar;
+		case 4: return Erdelementar;
+		}
+		return null;
+		
 	}
 }
