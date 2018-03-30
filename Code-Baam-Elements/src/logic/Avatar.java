25195d5a693d734cb8a7e4145d9c72f106400817
simonjaeggi <dev.simon.jaeggi@gmail.com>
2018-03-30 15:09:53 +0200

    setLebenspunkte wird nicht benötigt, da die LP pro Avatar fix sind.

--- Code-Baam-Elements/src/logic/Avatar.java
+++ Code-Baam-Elements/src/logic/Avatar.java
@@ -8,13 +8,18 @@ package logic;
 public class Avatar{
 	private double Lebenspunkte;
 	private Angriffsverhalten angriff;
-
+	
+	/**
+	 * 
+	 * @param zw
+	 * Zieht den mitgelieferten Wert von den lokalen Lebenspunkten ab.
+	 */
 	public void WenigerLebenspunkte(double zw){
 		Lebenspunkte = Lebenspunkte - zw;
 	}
-	protected void setLebenspunkte(double Lp){
-		this.Lebenspunkte = Lebenspunkte;
-	}
+	
+
+
 	public double getLebenspunkte(){
 		return Lebenspunkte;
 	}
