63a0cbb0e9dfbe2e6daff5c9daa914bfef9df5b5
simonjaeggi <dev.simon.jaeggi@gmail.com>
2018-03-30 09:32:39 +0200

    Fehler in Avatar.WenigerLebenspunkte() behoben. Kampf.Angreifen() angepasst.

--- Code-Baam-Elements/src/logic/Avatar.java
+++ Code-Baam-Elements/src/logic/Avatar.java
@@ -1,11 +1,16 @@
 package logic;
-
+/**
+ * 
+ * @author Simon Jäggi, Lukas Reinhardt
+ * @version 0.1
+ * Oberklasse für die einzelnen Avatare, vereint alle gemeinsamen Attribute.
+ */
 public class Avatar{
 	private double Lebenspunkte;
 	private Angriffsverhalten angriff;
 
-	public void WenigerLebenspunkte(){
-
+	public void WenigerLebenspunkte(double zw){
+		Lebenspunkte = Lebenspunkte - zw;
 	}
 	protected void setLebenspunkte(double Lp){
 		this.Lebenspunkte = Lebenspunkte;
--- Code-Baam-Elements/src/logic/Kampf.java
+++ Code-Baam-Elements/src/logic/Kampf.java
@@ -20,6 +20,7 @@ public class Kampf {
 		double zw = Avatar_1.AngreifenAufrufen();
 		if(Avatar_1 == StarkerAvatar){
 			zw = zw * 1.2;
+			Avatar_2.WenigerLebenspunkte(zw);
 		}
 		if (gestorben(Avatar_2) == true){
 			gewonnen();
@@ -28,6 +29,7 @@ public class Kampf {
 		zw = Avatar_2.AngreifenAufrufen();
 		if(Avatar_2 == StarkerAvatar){
 			zw = zw * 1.2;
+			Avatar_1.WenigerLebenspunkte(zw);
 		}
 		if (gestorben(Avatar_1) == true){
 			verloren();
