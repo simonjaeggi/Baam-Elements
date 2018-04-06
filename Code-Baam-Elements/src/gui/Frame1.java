8d3041f07a8d882fba89b60a20c6c68e2906be50
reinhlu1 <reinhlu1@ICHALW-NC00190.idorsia.com>
2018-04-06 14:20:02 +0200

    Update on Gui frames

--- Code-Baam-Elements/src/gui/Frame1.java
+++ Code-Baam-Elements/src/gui/Frame1.java
@@ -7,8 +7,17 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import java.awt.Color;
+
+/**
+ * 
+ * @author Simon Jaeggi, Lukas Reinhardt
+ * Die Klasse ist die Start-Oberfläche des Spiels, der Eigene Avatar kann hier ausgewählt werden.
+ */
+
 
 public class Frame1 {
 
@@ -47,27 +56,43 @@ public class Frame1 {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+		//Button Feueravatar
 		JButton FeuerElementar = new JButton("Feuer");
+		FeuerElementar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		FeuerElementar.setForeground(new Color(255, 255, 255));
 		FeuerElementar.setBackground(new Color(128, 0, 0));
 		FeuerElementar.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		FeuerElementar.setBounds(10, 451, 261, 73);
 		frame.getContentPane().add(FeuerElementar);
 		
+		//Button Wasseravatar
 		JButton WasserElementar = new JButton("Wasser");
+		WasserElementar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		WasserElementar.setForeground(new Color(255, 255, 255));
 		WasserElementar.setBackground(new Color(0, 0, 255));
 		WasserElementar.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		WasserElementar.setBounds(286, 451, 261, 73);
 		frame.getContentPane().add(WasserElementar);
 		
+		//Button Erdavatar
 		JButton ErdElementar = new JButton("Erde");
+		ErdElementar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		ErdElementar.setForeground(new Color(255, 255, 255));
 		ErdElementar.setBackground(new Color(153, 102, 0));
 		ErdElementar.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		ErdElementar.setBounds(562, 451, 261, 73);
 		frame.getContentPane().add(ErdElementar);
 		
+		//Button Luftavatar
 		JButton LuftElementar = new JButton("Luft");
 		LuftElementar.setBackground(new Color(135, 206, 235));
 		LuftElementar.setForeground(new Color(255, 255, 255));
@@ -78,18 +103,39 @@ public class Frame1 {
 		});
 		LuftElementar.setBounds(838, 451, 261, 73);
 		frame.getContentPane().add(LuftElementar);
+	
+		//Button Zufälliger Avatar auswählen
+		JButton RandomAvatar = new JButton("Zufalls Avatar");
+		RandomAvatar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("ZUFALLL");
+			}
+		});
+		RandomAvatar.setForeground(Color.WHITE);
+		RandomAvatar.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		RandomAvatar.setBackground(new Color(255, 0, 255));
+		RandomAvatar.setBounds(453, 383, 215, 57);
+		frame.getContentPane().add(RandomAvatar);
 		
+		//Titel "Wähle deinen Avatar"
 		JLabel lblWhleDeinenAvatar = new JLabel("W\u00E4hle deinen Avatar:");
 		lblWhleDeinenAvatar.setForeground(new Color(230, 230, 250));
 		lblWhleDeinenAvatar.setFont(new Font("Tahoma", Font.PLAIN, 75));
 		lblWhleDeinenAvatar.setBounds(197, 170, 721, 91);
 		frame.getContentPane().add(lblWhleDeinenAvatar);
 		
-		JButton RandomAvatar = new JButton("Zufalls Avatar");
-		RandomAvatar.setForeground(Color.WHITE);
-		RandomAvatar.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		RandomAvatar.setBackground(new Color(255, 0, 255));
-		RandomAvatar.setBounds(453, 383, 215, 57);
-		frame.getContentPane().add(RandomAvatar);
+		//helpbox
+		JButton help = new JButton("?");
+		help.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Wähle den Avatar aus mit dem du dich ins Gefecht stürzen möchtest!");
+			}
+		});
+		help.setFont(new Font("Tahoma", Font.BOLD, 19));
+		help.setBounds(21, 11, 48, 43);
+		frame.getContentPane().add(help);
+		
+				
+		
 	}
 }
--- Code-Baam-Elements/src/gui/Frame2.java
+++ Code-Baam-Elements/src/gui/Frame2.java
@@ -3,8 +3,13 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import java.awt.Color;
+
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import java.awt.Font;
 
 public class Frame2 {
 
@@ -34,7 +39,7 @@ public class Frame2 {
 	}
 
 	/**
-	 * Initialize the contents of the frame.
+	 * Inizialisierung des Frames
 	 */
 	private void initialize() {
 		frame = new JFrame();
@@ -43,13 +48,80 @@ public class Frame2 {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(40, 536, 219, 44);
+		
+		//Titel
+		JLabel lblKampf = new JLabel("Kampf");
+		lblKampf.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 70));
+		lblKampf.setForeground(new Color(255, 255, 255));
+		lblKampf.setBounds(54, 34, 546, 109);
+		frame.getContentPane().add(lblKampf);
+		
+	//Spieler1:
+		//Button Schlag
+		JButton btnNewButton = new JButton("Schlag");
+		btnNewButton.setBackground(new Color(255, 255, 255));
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 35));
+		btnNewButton.setBounds(54, 525, 256, 51);
 		frame.getContentPane().add(btnNewButton);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(40, 237, 219, 218);
+		//Bild für Avatar
+		JLabel Spieler_Avatar_Lable = new JLabel();
+		Spieler_Avatar_Lable.setIcon(new ImageIcon(this.getClass().getResource("/data/Feuer_Avatar.png")));
+		Spieler_Avatar_Lable.setBounds(54, 225, 256, 256);
+		frame.getContentPane().add(Spieler_Avatar_Lable);
+		
+		//Lebensanzeige
+		JLabel Lebensanzeige = new JLabel("100");
+		Lebensanzeige.setFont(new Font("Tahoma", Font.BOLD, 32));
+		Lebensanzeige.setForeground(new Color(255, 255, 255));
+		Lebensanzeige.setBackground(new Color(255, 255, 255));
+		Lebensanzeige.setBounds(414, 330, 60, 46);
+		frame.getContentPane().add(Lebensanzeige);
+		
+		//Hintergrund für Lebensanzeige(Herz)
+		JLabel Lebensanzeige_Hintergrund = new JLabel();
+		Lebensanzeige_Hintergrund.setIcon(new ImageIcon(this.getClass().getResource("/data/Lebensanzeige_Hintergrund.png")));
+		Lebensanzeige_Hintergrund.setBounds(380, 289, 128, 128);
+		frame.getContentPane().add(Lebensanzeige_Hintergrund);
+		
+		
+		
+	//Spieler2:
+		//Bild für Gegnerischen Avatar
+		JLabel Spieler2_Avatar_Lable = new JLabel("");
+		Spieler2_Avatar_Lable.setBounds(824, 105, 128, 128);
+		frame.getContentPane().add(Spieler2_Avatar_Lable);
+		
+		//Lebensanzeige für Gegneravatar
+		JLabel lblNewLabel = new JLabel("100");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		lblNewLabel.setBounds(635, 174, 46, 14);
 		frame.getContentPane().add(lblNewLabel);
+		
+		//Hintergrund für Lebensanzeige(Herz)
+		JLabel Spieler2_Lebensanzeige_Hintergrund = new JLabel();
+		Spieler2_Lebensanzeige_Hintergrund.setIcon(new ImageIcon(this.getClass().getResource("/data/Lebensanzeige_Hintergrund.png")));
+		/*
+		ImageIcon Spieler2_Lebensanzeige_Hintergrund_resized = new ImageIcon();
+		Image img = Spieler2_Lebensanzeige_Hintergrund_resized.getImage(); 
+        Image newimg = img.getScaledInstance(Spieler2_Lebensanzeige_Hintergrund.getWidth(), Spieler2_Lebensanzeige_Hintergrund.getHeight(),  java.awt.Image.SCALE_SMOOTH);
+        Spieler2_Lebensanzeige_Hintergrund.setIcon(Spieler2_Lebensanzeige_Hintergrund_resized);
+	
+		 /* BufferedImage img = null;
+		try {
+			img = ImageIO.read(new File("/data/Lebensanzeige_Hintergrund.png"));
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+		Image dimg = img.getScaledInstance(Spieler2_Lebensanzeige_Hintergrund.getWidth(), Spieler2_Lebensanzeige_Hintergrund.getHeight(),
+		        Image.SCALE_SMOOTH);
+		ImageIcon Spieler2_Lebensanzeige_Hintergrund_resized = new ImageIcon(dimg);
+		*/
+		Spieler2_Lebensanzeige_Hintergrund.setBounds(728, 135, 64, 64);
+		frame.getContentPane().add(Spieler2_Lebensanzeige_Hintergrund);
+		
+		
+		
 	}
-
 }
