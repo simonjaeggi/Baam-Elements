f548bd4dcd03c7e54d0dec20fe4af83a4ee4aa8e
simon <simon@PCMASTERRACE123>
2018-04-17 21:03:24 +0200

    javadoc angepasst

--- Code-Baam-Elements/src/data/AvatarStaerke.java
+++ Code-Baam-Elements/src/data/AvatarStaerke.java
@@ -12,14 +12,25 @@ import logic.*;
 public class AvatarStaerke {
 	private Avatar Avatar_1;
 	private Avatar Avatar_2;
-
+	/**
+	 * 
+	 * @param Avatar_1
+	 * @param Avatar_2
+	 * Konstruktor, hier werden die beiden Avatar Objekte mitgegeben und in die lokalen Variablen gespeichert 
+	 */
 	public AvatarStaerke(Avatar Avatar_1, Avatar Avatar_2){
-
+		this.Avatar_1 = Avatar_1;
+		this.Avatar_2 = Avatar_2;
 	}
+	/**
+	 * 
+	 * @return zw
+	 * Hier wird auf die einzelnen staerken der Avatare ueberpreueft. Wenn ein Avatar staerker ist, wird er mit dem return Wert zw zurueckgegeben
+	 */
 	public Avatar Staerkenvergleich(){
 		Avatar zw = null;
 		
-		//Geprüft ob Feuerelementar gegen Erdelementar im Spiel ist
+
 			if(Avatar_1 instanceof Feuerelementar || Avatar_1 instanceof Erdelementar && Avatar_2 instanceof Feuerelementar || Avatar_2 instanceof Erdelementar){
 				if(Avatar_1 instanceof Feuerelementar){
 					return Avatar_1;
@@ -27,7 +38,7 @@ public class AvatarStaerke {
 					return Avatar_2;
 				}
 			}
-		//Geprüft ob Erdelementar gegen luftelementar im Spiel ist
+
 			else if(Avatar_1 instanceof Erdelementar || Avatar_1 instanceof Luftelementar && Avatar_2 instanceof Erdelementar || Avatar_2 instanceof Luftelementar){
 				if(Avatar_1 instanceof Erdelementar){
 					return Avatar_1;
@@ -35,7 +46,7 @@ public class AvatarStaerke {
 					return Avatar_2;
 				}
 			}
-		//Geprüft ob Luftelementar geglxlxlxen Wasserelementar im Spiel ist
+
 			else if(Avatar_1 instanceof Luftelementar || Avatar_1 instanceof Wasserelementar && Avatar_2 instanceof Luftelementar || Avatar_2 instanceof Wasserelementar){
 				if(Avatar_1 instanceof Luftelementar){
 					return Avatar_1;
@@ -43,7 +54,7 @@ public class AvatarStaerke {
 					return Avatar_2;
 				}
 			}
-		//Geprüft ob Wasserelementar gegen Feuerelementar im Spiel ist
+
 			else if(Avatar_1 instanceof Wasserelementar || Avatar_1 instanceof Feuerelementar && Avatar_2 instanceof Wasserelementar || Avatar_2 instanceof Feuerelementar){
 				if(Avatar_1 instanceof Wasserelementar){
 					return Avatar_1;
