72026799e27965ed60dbf68f383f65f6be3b567b
reinhlu1 <reinhlu1@ICHALW-NC00190.idorsia.com>
2018-04-18 12:30:20 +0200

    javadoc optimiert

--- Code-Baam-Elements/src/logic/Avatar.java
+++ Code-Baam-Elements/src/logic/Avatar.java
@@ -18,16 +18,24 @@ public class Avatar{
 	}
 	
 	/**
-	 * 
+	 * Setzt lokale Variable "Lebenspunkte" auf den mitgelieferten Wert 
 	 * @param Lp
 	 * 
 	 */
 	protected void setLebenspunkte(double Lp){
 		this.Lebenspunkte = Lp;
 	}
+	/**
+	 * Gibt den Wert der lokalen Variable Lebenspunkte zurück
+	 * @return
+	 */
 	public double getLebenspunkte(){
 		return Lebenspunkte;
 	}
+	/**
+	 * Diese Methode wird verwendet um die jeweilige Attacke aufzurufen. Da wir nur einen Angriff haben, ist das ganze statisch programmiert.
+	 * @return
+	 */
 	public double AngreifenAufrufen(){
 		angriff = new Schlag();
 		double zw = angriff.angreifen();
